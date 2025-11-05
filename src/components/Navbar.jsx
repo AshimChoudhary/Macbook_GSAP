7b0f91ef5a4c4327a242b84b15213f5a7df6acf9
Ashim Choudhary <choudharyashim99@gmail.com>
2025-11-05 15:52:32 +0530

    Navbar Basics

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+
+        <ul>
+          {[
+            { label: "Store" },
+            { label: "Mac" },
+            { label: "iPhone" },
+            { label: "Watch" },
+            { label: "Vision" },
+            { label: "AirPods" },
+          ].map((link) => (
+            <li key={link.label}>
+              <a href={link.label}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
