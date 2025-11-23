89c885756facd16047f09be4a746690bcc5ac731
Ashim Choudhary <choudharyashim99@gmail.com>
2025-11-23 19:25:42 +0530

    Highlight Section Minor Changes

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,16 +1,15 @@
+import { useMediaQuery } from "react-responsive";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import React from "react";
-import { useMediaQuery } from "react-responsive";
 
 const Highlights = () => {
-  const isMobile = useMediaQuery({ query: "(max-width:1024px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
 
   useGSAP(() => {
-    gsap.to([".left-cloumn", ".right-column"], {
+    gsap.to([".left-column", ".right-column"], {
       scrollTrigger: {
         trigger: "#highlights",
-        start: isMobile ? "bottom bottom" : "top top",
+        start: isMobile ? "bottom bottom" : "top center",
       },
       y: 0,
       opacity: 1,
@@ -22,42 +21,39 @@ const Highlights = () => {
 
   return (
     <section id="highlights">
-      <h2>There's never been a better time to upgrade</h2>
-      <h3>Here's what you getwith the new MacBook Pro</h3>
+      <h2>There’s never been a better time to upgrade.</h2>
+      <h3>Here’s what you get with the new MacBook Pro.</h3>
 
       <div className="masonry">
-        <div className="left-cloumn">
+        <div className="left-column">
           <div>
             <img src="/laptop.png" alt="Laptop" />
             <p>Fly through demanding tasks up to 9.8x faster.</p>
           </div>
-
           <div>
-            <img src="/sun.png" alt="sun" />
+            <img src="/sun.png" alt="Sun" />
             <p>
               A stunning <br />
               Liquid Retina XDR <br />
-              display
+              display.
             </p>
           </div>
         </div>
-
-        <div className="right-cloumn">
+        <div className="right-column">
           <div className="apple-gradient">
             <img src="/ai.png" alt="AI" />
             <p>
               Built for <br />
-              <span>Apple Inteligence.</span>
+              <span>Apple Intelligence.</span>
             </p>
           </div>
-
           <div>
             <img src="/battery.png" alt="Battery" />
             <p>
               Up to
               <span className="green-gradient"> 14 more hours </span>
               battery life.
-              <span className="text-dark-100"> (Up to 24 hours total)</span>
+              <span className="text-dark-100"> (Up to 24 hours total.)</span>
             </p>
           </div>
         </div>
@@ -65,5 +61,4 @@ const Highlights = () => {
     </section>
   );
 };
-
 export default Highlights;
